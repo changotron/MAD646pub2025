01bb7995541e8c690f232cf8d44f7a320fa5e8cd
changotron <lopez.michael2@gmail.com>
2025-03-23 10:56:38 +0900

    End week 3, adding onsen ons-splitter and js to handle button nav

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -4,11 +4,24 @@
     to create "real" apps via web code. 
 */
 
-    ons.ready(function() {
-      console.log("Onsen UI is ready!");
-    });
+// Onsen framework has successfully loaded onto my phone
+ons.ready(function() {
+    console.log("Onsen UI is ready!");
+});
 
-    if (ons.platform.isIPhoneX()) {
-      document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
-      document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
-    }
+//Check if running on certain device and then ....
+if (ons.platform.isIPhoneX()) {
+    document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+    document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+    console.log("running on iPhone X");
+} else if(ons.platform.isAndroid()){
+    console.log("running on android!");
+};
+
+function pageLoader(pageID){
+    console.log("pageLoader() is running, about to load: " + pageID);
+    document.querySelector("#appNav").resetToPage(pageID);
+}; // END pageLoader()
+
+// This loader is can run multiple pages
+pageLoader("welcome.html");
